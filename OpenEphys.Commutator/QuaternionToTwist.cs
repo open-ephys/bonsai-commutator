7e7e0ed954dc30137bfb724069bdf5c0dbd1d9dd
jonnew <jpn@open-ephys.org>
2024-11-01 12:34:09 -0400

    Make QuaternionToTwist.RotationAxis configuration property
    - Mark its category as such
    - Rename some of the internal variables in QuaternionToTwist to reflect
      their true meaning

--- OpenEphys.Commutator/QuaternionToTwist.cs
+++ OpenEphys.Commutator/QuaternionToTwist.cs
@@ -23,6 +23,7 @@ namespace OpenEphys.Commutator
         /// in the direction that the tether exits the headstage. Note that negating this vector will result in
         /// negating the direction of twisting.
         /// </remarks>
+        [Category(Definitions.ConfigurationCategory)]
         [TypeConverter(typeof(NumericRecordConverter))]
         [Description("The direction vector specifying the axis around which to calculate the twist.")]
         public Vector3 RotationAxis { get; set; } = Vector3.UnitZ;
@@ -35,30 +36,31 @@ namespace OpenEphys.Commutator
         /// <returns>The sequence of twist values, in units of turns.</returns>
         public override IObservable<double> Process(IObservable<Quaternion> source)
         {
+            var rotationAxis = RotationAxis;
+
             return Observable.Defer(() =>
             {
                 double? previousTwist = default;
                 return source.Select(rotation =>
                 {
                     // project rotation axis onto the direction axis
-                    var direction = RotationAxis;
-                    var rotationAxis = new Vector3(rotation.X, rotation.Y, rotation.Z);
-                    var dotProduct = Vector3.Dot(rotationAxis, direction);
-                    var projection = dotProduct / Vector3.Dot(direction, direction) * direction;
-                    var twist = new Quaternion(projection, rotation.W);
-                    twist = Quaternion.Normalize(twist);
+                    var vectorPart = new Vector3(rotation.X, rotation.Y, rotation.Z);
+                    var dotProduct = Vector3.Dot(vectorPart, rotationAxis);
+                    var projection = dotProduct / Vector3.Dot(rotationAxis, rotationAxis) * rotationAxis;
+                    var rotationAboutAxis = new Quaternion(projection, rotation.W);
+                    rotationAboutAxis = Quaternion.Normalize(rotationAboutAxis);
                     if (dotProduct < 0) // account for angle-axis flipping
                     {
-                        twist = -twist;
+                        rotationAboutAxis = -rotationAboutAxis;
                     }
 
                     // normalize twist feedback in units of turns
-                    var twistAngle = 2 * Math.Acos(twist.W);
-                    var feedback = previousTwist.HasValue
-                        ? (twistAngle - previousTwist.GetValueOrDefault() + 3 * Math.PI) % (2 * Math.PI) - Math.PI
+                    var angleAboutAxis = 2 * Math.Acos(rotationAboutAxis.W);
+                    var twist = previousTwist.HasValue
+                        ? (angleAboutAxis - previousTwist.GetValueOrDefault() + 3 * Math.PI) % (2 * Math.PI) - Math.PI
                         : 0;
-                    previousTwist = twistAngle;
-                    return -feedback / (2 * Math.PI);
+                    previousTwist = angleAboutAxis;
+                    return -twist / (2 * Math.PI);
                 });
             });
         }
